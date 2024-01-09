4a10efa33b3e8737279afa290f2ae2b4491c5a5c
Paul Davis <paul@linuxaudiosystems.com>
2024-01-08 22:35:37 -0700

    initial refactoring of MidiRegionView IS-A MidiView

--- gtk2_ardour/audio_region_view.h
+++ gtk2_ardour/audio_region_view.h
@@ -220,7 +220,7 @@ protected:
 	void transients_changed();
 
 	AutomationLine::VisibleAspects automation_line_visibility () const;
-	void _redisplay (bool) {}
+	void redisplay (bool) {}
 
 private:
 	void setup_fade_handle_positions ();
